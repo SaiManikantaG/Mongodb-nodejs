478a4042b769e601c568c7eacd2748ebe2342409
Sai Manikanta Godavarthi <saig2304@gmail.com>
2021-05-07 20:40:35 -0500

    Create updateAllListings.js

---
+++ updateAllListings.js
@@ -0,0 +1,11 @@
+async function updateAllListingsToHavePropertyType(client) {
+  const result = await client
+    .db("playground")
+    .collection("sample")
+    .updateMany(
+      { property_type: { $exists: false } },
+      { $set: { property_type: "Unknown" } }
+    );
+  console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+  console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
